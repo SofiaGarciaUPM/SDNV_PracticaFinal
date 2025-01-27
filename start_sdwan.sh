3a673a5f58db0fa37734098ff0c7bc2a8cb05466
SofiaGarciaUPM <sofia.garcia.rincon@alumnos.upm.es>
2025-01-27 13:27:43 +0100

    brwan y controller

--- start_sdwan.sh
+++ start_sdwan.sh
@@ -12,9 +12,17 @@ set -u # to verify variables are defined
 : $KUBECTL
 : $SDWNS
 : $NETNUM
+: $VACC
 : $VCPE
 : $VWAN
+: $VRYU
 : $REMOTESITE
+
+if [[ ! $VWAN =~ "-accchart"  ]]; then
+   echo ""       
+   echo "ERROR: incorrect <acc_deployment_id>: $VWAN"
+   exit 1
+fi
 
 if [[ ! $VCPE =~ "-cpechart"  ]]; then
     echo ""       
@@ -28,9 +36,17 @@ if [[ ! $VWAN =~ "-wanchart"  ]]; then
    exit 1
 fi
 
+if [[ ! $VWAN =~ "-ryuchart"  ]]; then
+   echo ""       
+   echo "ERROR: incorrect <ryu_deployment_id>: $VWAN"
+   exit 1
+fi
+
+ACC_EXEC="$KUBECTL exec -n $SDWNS $VACC --"
 CPE_EXEC="$KUBECTL exec -n $SDWNS $VCPE --"
 WAN_EXEC="$KUBECTL exec -n $SDWNS $VWAN --"
 WAN_SERV="${VWAN/deploy\//}"
+RYU_EXEC="$KUBECTL exec -n $SDWNS $VRYU --"
 
 # Router por defecto inicial en k8s (calico)
 K8SGW="169.254.1.1"
@@ -38,11 +54,17 @@ K8SGW="169.254.1.1"
 ## 1. Obtener IPs y puertos de las VNFs
 echo "## 1. Obtener IPs y puertos de las VNFs"
 
+IPACC=`$ACC_EXEC hostname -I | awk '{print $1}'`
+echo "IPACC = $IPACC"
+
 IPCPE=`$CPE_EXEC hostname -I | awk '{print $1}'`
 echo "IPCPE = $IPCPE"
 
 IPWAN=`$WAN_EXEC hostname -I | awk '{print $1}'`
 echo "IPWAN = $IPWAN"
+
+IPRYU=`$RYU_EXEC hostname -I | awk '{print $1}'`
+echo "IPRYU = $IPRYU"
 
 PORTWAN=`$KUBECTL get -n $SDWNS -o jsonpath="{.spec.ports[0].nodePort}" service $WAN_SERV`
 echo "PORTWAN = $PORTWAN"
@@ -54,6 +76,13 @@ $CPE_EXEC ovs-vsctl add-br brwan
 $CPE_EXEC ip link add cpewan type vxlan id 5 remote $IPWAN dstport 8741 dev eth0
 $CPE_EXEC ovs-vsctl add-port brwan cpewan
 $CPE_EXEC ifconfig cpewan up
+
+
+$CPE_EXEC ovs-vsctl set bridge brwan protocols=OpenFlow10,OpenFlow12,OpenFlow13
+$CPE_EXEC ovs-vsctl set-fail-mode brwan secure
+$CPE_EXEC ovs-vsctl set bridge brwan other-config:datapath-id=0000000000000002
+$CPE_EXEC ovs-vsctl set-controller brwan tcp:$IPRYU:6633
+
 $CPE_EXEC ip link add sr1sr2 type vxlan id 12 remote $REMOTESITE dstport 8742 dev net$NETNUM
 $CPE_EXEC ovs-vsctl add-port brwan sr1sr2
 $CPE_EXEC ifconfig sr1sr2 up
@@ -70,14 +99,22 @@ echo "## 4. En VNF:wan activar el modo SDN del conmutador y crear vxlan"
 $WAN_EXEC ovs-vsctl set bridge brwan protocols=OpenFlow10,OpenFlow12,OpenFlow13
 $WAN_EXEC ovs-vsctl set-fail-mode brwan secure
 $WAN_EXEC ovs-vsctl set bridge brwan other-config:datapath-id=0000000000000001
-$WAN_EXEC ovs-vsctl set-controller brwan tcp:127.0.0.1:6633
+$WAN_EXEC ovs-vsctl set-controller brwan tcp:$IPRYU:6633
 
 $WAN_EXEC ip link add cpewan type vxlan id 5 remote $IPCPE dstport 8741 dev eth0
 $WAN_EXEC ovs-vsctl add-port brwan cpewan
 $WAN_EXEC ifconfig cpewan up
 
-## 5. Aplica las reglas de la sdwan con ryu
-echo "## 5. Aplica las reglas de la sdwan con ryu"
+##5. Activar el nodo SDN en VNF:access
+echo "##5. Activar el nodo SDN en VNF:access"
+$ACC_EXEC ovs-vsctl set bridge brwan protocols=OpenFlow10, OpenFlow12, OpenFlow13
+$ACC_EXEC ovs-vsctl set-fail-mode brwan secure 
+$ACC_EXEC ovs-vsctl set bridge other-config:datapath-id=0000000000000003
+$ACC_EXEC ovs-vsctl set-controller brwan tcp:$IPRYU:6633
+$ACC_EXEC ovs-vsctl set-manager ptcp:6632
+
+## 6. Aplica las reglas de la sdwan con ryu
+echo "## 6. Aplica las reglas de la sdwan con ryu"
 RYU_ADD_URL="http://localhost:$PORTWAN/stats/flowentry/add"
 curl -X POST -d @json/from-cpe.json $RYU_ADD_URL
 curl -X POST -d @json/to-cpe.json $RYU_ADD_URL
